c492c1f9f8f6037daca76977cac21986ff4d4bdb
Oleg Haidul <oleghaidul@gmail.com>
2012-01-17 19:41:04 +0300

    f

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -2,7 +2,7 @@ module ItemsHelper
   def next_item(id)
     unless id == Item.last.id
       if Item.find(id)
-        link_to "", item_path(id+1), :remote => true
+        link_to "", item_path(id+1), :remote => true, :html => {:"data-spinner" => "#user_form_spinner"}
       else
         next_item(id+1)
       end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$("*[data-spinner]").live('ajax:beforeSend', function(e){
+  $($(this).data('spinner')).show();
+  e.stopPropagation(); //Don't show spinner of parent elements.
+});
+$("*[data-spinner]").live('ajax:complete', function(){
+  $($(this).data('spinner')).hide();
+});
