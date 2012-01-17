1bd7812889a56eb94305e009691986bba596897d
Oleg Haidul <oleghaidul@gmail.com>
2012-01-17 20:01:55 +0300

    spinner

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-$("*[data-spinner]").live('ajax:beforeSend', function(e){
-  $($(this).data('spinner')).show();
-  e.stopPropagation(); //Don't show spinner of parent elements.
-});
-$("*[data-spinner]").live('ajax:complete', function(){
-  $($(this).data('spinner')).hide();
+$(function() {
+    $('#spinner')
+        .hide() // hide initially
+        .ajaxStart(function(){
+            $(this).show();
+        })
+        .ajaxStop(function(){
+            $(this).hide();
+        })
 });
