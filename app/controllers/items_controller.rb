eecdc76c323e3252059a22c6ffb3926c433c3618
Oleg Haidul <oleghaidul@gmail.com>
2012-01-18 09:06:27 +0300

    load_item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 	def show
 		@item = Item.find(params[:id])
 		respond_to do |format|
-      format.html 
+      format.html {render :layout => false}
       format.js
     end
 	end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,5 +11,24 @@ $(function() {
         })
 });
 
+$(document).ready(function() {
+  $('#load_item').click(function(e) {
+    var url = $(this).attr('href');
+    var dialog_form = $('<div id="dialog-form">Loading form...</div>').dialog({
+      autoOpen: false,
+      width: 1220,
+      height: 600,
+      modal: true,
+      open: function() {
+        return $(this).load(url + ' #content');
+      },
+      close: function() {
+        $('#dialog-form').remove();
+      }
+    });
+    dialog_form.dialog('open');
+    e.preventDefault();
+  });
+});
 
 
