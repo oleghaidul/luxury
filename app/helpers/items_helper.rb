d865ef460b4e4887e67f88d548f96c92c9201f64
Oleg Haidul <oleghaidul@gmail.com>
2012-01-20 14:22:29 +0300

    fix height

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -2,7 +2,7 @@ module ItemsHelper
   def next_item(id, brand)
     unless id == brand.items.last.id
       next_item = brand.items.where("items.id > ?", id).first
-      link_to image_tag("rails.png"), item_path(next_item.id), :remote => true
+      link_to image_tag("arrows_05.png", :size => "90x460"), item_path(next_item.id), :remote => true
     else
       link_to "&rArr;".html_safe,"#", :class => "arrow_end"
     end
@@ -11,7 +11,7 @@ module ItemsHelper
   def prev_item(id,brand)
     unless id == brand.items.first.id
       prev_item = brand.items.where("items.id < ?", id).last
-      link_to image_tag("rails.png"), item_path(prev_item.id), :remote => true
+      link_to image_tag("arrows_03.png", :size => "90x460"), item_path(prev_item.id), :remote => true
     else
       link_to "&lArr;".html_safe, "#", :class => "arrow_end"
     end
